3e088715d4736d5c1b0f2671932c0f1955fb30cf
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-15 11:33:28 +0100

    chore: movie template

--- movies/settings.py
+++ movies/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "movies", # Add this line to make template accessible
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def movies(request):
-    return HttpResponse('Movie...')
+    return render(request, 'movies/movies.html', {'movies': ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight']})
 
 def home(request):
     return HttpResponse('Home...')
