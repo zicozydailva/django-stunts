e55882c4baf5fb99684d47f6b08e9eceb697e0cb
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-15 11:48:27 +0100

    chore: movie template formatting

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 data = {
-    'movies': ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight']
+    'movies': [{'id': 1, 'title': 'The Shawshank Redemption', 'year': 1994}, {'id:': 2, 'title': 'The Godfather', 'year': 1972}, {'id': 3, 'title': 'The Dark Knight', 'year': 2008}, {'id:': 4, 'title': 'The Lord of the Rings: The Return of the King', 'year': 2003}, {'id': 5, 'title': 'Pulp Fiction', 'year': 1994}]
 }
 
 def movies(request):
