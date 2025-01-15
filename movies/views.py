22752a4f04430464ea2a346e6f6e00ea8ee35d60
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-15 11:36:04 +0100

    chore: movie template

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data = {
+    'movies': ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight']
+}
+
 def movies(request):
-    return render(request, 'movies/movies.html', {'movies': ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight']})
+    return render(request, 'movies/movies.html', data) # pointing to movies/movies.html template
 
 def home(request):
     return HttpResponse('Home...')
